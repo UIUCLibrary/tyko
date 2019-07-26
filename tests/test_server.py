14e1b218bd4644fcdb69145850bafef92b360432
henry <henrybochers@yahoo.com>
2019-07-26 15:23:22 -0500

    test server static pages and api enum

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,15 +1,40 @@
 import avforms
 import pytest
+import json
 
-
-@pytest.fixture()
+@pytest.fixture(scope="module")
 def server():
     app = avforms.create_app("sqlite:///:memory:")
     app.config["TESTING"] = True
-    yield app.test_client()
-    print("Tearing down")
+    return app.test_client()
+
+@pytest.mark.parametrize(
+    "route", ["/",
+              "/about",
+              "/api",
+              "/api/formats",
+              "/api/projects",
+              "/api/collections",
+              ]
+)
+def test_static(route):
+    app = avforms.create_app("sqlite:///:memory:")
+    app.config["TESTING"] = True
+    with app.test_client() as server:
+        resp = server.get(route)
+        assert resp.status == "200 OK"
 
 
-def test_root(server):
-    resp = server.get("/")
+
+def test_api_formats(server):
+    resp = server.get("/api/formats")
     assert resp.status == "200 OK"
+    tmp_data = json.loads(resp.data)
+
+    for k, v in  avforms.scheme.format_types.items():
+        for entry in tmp_data:
+            if entry["name"] == k:
+                assert entry["id"] == v[0]
+                break
+        else:
+            assert False
