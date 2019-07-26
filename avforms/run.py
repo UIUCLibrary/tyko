ee2c0fce65a80b5161306d5e64f600e4f0f0a45a
henry <henrybochers@yahoo.com>
2019-07-26 15:23:43 -0500

    don't use app as a global var

--- avforms/run.py
+++ avforms/run.py
@@ -4,18 +4,17 @@ from flask import Flask
 from avforms import routes
 from avforms.config import setup_cli_parser
 
-app = Flask(__name__)
 
-
-def create_app(db_engine):
-    app_routes = routes.Routes(db_engine, app=app)
+def create_app(db_engine, app=None):
+    if app is None:
+        app = Flask(__name__)
+    app_routes = routes.Routes(db_engine, app)
     if not app_routes.is_valid():
         sys.exit(1)
 
     app_routes.init_api_routes()
-    routes.init_website_routes(app, db_engine)
+    app_routes.init_website_routes()
     return app
-    # app.run(host='0.0.0.0')
 
 
 def main() -> None:
@@ -26,6 +25,5 @@ def main() -> None:
 
     # Validate that the database can be connected to
     print(args)
-
     my_app = create_app(args.db_engine)
     my_app.run()
