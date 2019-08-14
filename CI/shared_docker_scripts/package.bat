700de8dd8196b156c18d3ac4c385cc187bf4c640
henry <henryborchers@yahoo.com>
2019-08-14 11:05:34 -0500

    multi package

--- CI/shared_docker_scripts/package.bat
+++ CI/shared_docker_scripts/package.bat
@@ -1,7 +1,7 @@
  setlocal
  if not exist c:\TEMP\build mkdir c:\TEMP\build
  xcopy c:\build c:\TEMP\build
- cd c:\TEMP\build && cpack -G NSIS -G WIX
+ cd c:\TEMP\build && cpack -G NSIS;WIX
  copy *.msi c:\dist\
  copy *.exe c:\dist\
  endlocal
