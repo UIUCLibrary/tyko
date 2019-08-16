a11ad68e05bb50a3770908c282f12b2dd46e55ef
henry <henrybochers@yahoo.com>
2019-08-16 15:19:43 -0500

    DIST_DIR

--- CI/shared_docker_scripts/package.bat
+++ CI/shared_docker_scripts/package.bat
@@ -1,8 +1,12 @@
- setlocal
- if not exist c:\TEMP\build mkdir c:\TEMP\build
- xcopy c:\build c:\TEMP\build
- cd c:\TEMP\build && cpack -G ZIP;NSIS;WIX
- copy *.msi c:\dist\
- copy *.exe c:\dist\
- copy *.zip c:\dist\
- endlocal
+@echo off
+setlocal
+set "starting_dir=%cd%"
+set "DIST_DIR=%cd%\dist\"
+if not exist c:\TEMP\build mkdir c:\TEMP\build
+xcopy .\build c:\TEMP\build
+cd c:\TEMP\build && cpack -G ZIP;NSIS;WIX
+if not exist %DIST_DIR% mkdir %DIST_DIR%
+copy *.msi %DIST_DIR%
+copy *.exe %DIST_DIR%
+copy *.zip %DIST_DIR%
+endlocal
