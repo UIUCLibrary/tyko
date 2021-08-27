1e9df3ef0a1c7e9495aef51ec2df6c309a2ad524
henry <henryborchers@yahoo.com>
2021-08-27 15:18:07 -0500

    TestProjectObjectAPI get

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,9 +1,12 @@
+from unittest.mock import Mock, MagicMock
+
 import pytest
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 import tyko.routes
 import tyko.database
+import tyko.views.project_object
 
 from tyko.data_provider import DataProvider
 
@@ -20,3 +23,29 @@ def test_get_api():
     routes = tyko.routes.Routes(data_provider, testing_app)
     for r in routes.get_api_routes():
         assert isinstance(r, tyko.routes.UrlRule)
+
+
+class TestProjectObjectAPI:
+    def test_get(self, monkeypatch):
+        def url_for(*args, **kwargs):
+            return ""
+
+        jsonify = Mock()
+        monkeypatch.setattr(tyko.views.project_object, "url_for", url_for)
+        monkeypatch.setattr(tyko.views.project_object, "jsonify", jsonify)
+        middle_ware = Mock()
+        project = {
+            "objects": [
+                {
+                    "items": [
+                        MagicMock(name="item")
+                    ],
+                    "object_id": "obj"
+                }
+            ],
+            "project_id": "dummy"
+        }
+        middle_ware.get_project_by_id = Mock(return_value=project)
+        view = tyko.views.project_object.ProjectObjectAPI(middle_ware)
+        view.get(project_id="dummy", object_id="obj")
+        assert jsonify.called is True
