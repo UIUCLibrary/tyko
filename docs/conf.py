55a73d5707a16c4967d5ee095b1d75f0ccbb4a28
henry <henryborchers@yahoo.com>
2022-01-24 11:53:33 -0600

    add UIUC logo

--- docs/conf.py
+++ docs/conf.py
@@ -66,6 +66,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+html_theme_options = {
+    "logo": "full_mark_horz_bw.gif",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
