b0183ef06702577f6e3451678bb5b881ae4fc61e
henry <henryborchers@yahoo.com>
2022-01-27 13:46:45 -0600

    test_get_notes_remove_parent_info

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -16,3 +16,17 @@ class TestProjectMiddlewareEntity:
         notes_middleware = Mock(spec=middleware.NotestMiddlwareEntity)
         notes_middleware.get = Mock(return_value=note_detail)
         assert [note_detail] == mw.serialize_notes(notes, notes_middleware)
+
+
+class TestNotestMiddlewareEntity:
+    def test_get_notes_remove_parent_info(self):
+        data_provider = Mock()
+        mw = middleware.NotestMiddlwareEntity(data_provider)
+        mw._data_connector.get = Mock(
+            return_value={
+                "parent_project_ids": [],
+                "parent_object_ids": [],
+                "parent_item_ids": [],
+            }
+        )
+        assert "parent_project_ids" not in mw.get(id=1, resolve_parents=False)
