e23e91f0c31dcead20e64d00c37320f3dcdd7e70
henry <henryborchers@yahoo.com>
2022-02-16 13:31:37 -0600

    formatting

--- tyko/static/js/tyko.mjs
+++ tyko/static/js/tyko.mjs
@@ -1,58 +1,48 @@
-import * as metadataWidgets from "./metadataWidgets.mjs";
+import * as metadataWidgets from './metadataWidgets.mjs';
 /**
  *  Assign element with tyko-editor class
  *  This provides saveEdit event
  * @param {HTMLDivElement} element
  */
 function configureTykoEditorTags(element) {
-    const saveNoteButton = element.querySelector(".tyko-accept")
-    let form = element.querySelector("form")
-    if(saveNoteButton) {
-
-        saveNoteButton.addEventListener("click", () => {
-            element.dispatchEvent(
-                new CustomEvent(
-                    'saveEdit',
-                    {
-                        detail: {
-                            elements: packageElements(form.elements),
-                            apiRoute: element.dataset.apiUrl
-                        }
-                    }
-                )
-            );
-        })
-    }
+  const saveNoteButton = element.querySelector('.tyko-accept');
+  const form = element.querySelector('form');
+  if (saveNoteButton) {
+    saveNoteButton.addEventListener('click', () => {
+      element.dispatchEvent(
+          new CustomEvent(
+              'saveEdit',
+              {
+                detail: {
+                  elements: packageElements(form.elements),
+                  apiRoute: element.dataset.apiUrl,
+                },
+              },
+          ),
+      );
+    });
+  }
 }
 
 /**
  * @param {HTMLFormControlsCollection} elements
  * @return {Object}
  */
-function packageElements(elements){
-    let data = {}
-    for (const element of elements) {
-        data[element.name] = element.value
-    }
-    return data
+function packageElements(elements) {
+  const data = {};
+  for (const element of elements) {
+    data[element.name] = element.value;
+  }
+  return data;
 }
 
 /**
  * Set elements used by Tyko
  */
-function setTykoClasses(){
-    for (const element of document.getElementsByClassName("tyko-editor")){
-        configureTykoEditorTags(element)
-    }
-    // for (const element of document.getElementsByClassName("tyko-date-selector")){
-    //     // $(element).ready(() => {
-    //     //     $(element).datepicker({
-    //     //         uiLibrary: 'bootstrap4',
-    //     //         format: 'yyyy-mm-dd'
-    //     //     });
-    //     // });
-    // }
-
+function setTykoClasses() {
+  for (const element of document.getElementsByClassName('tyko-editor')) {
+    configureTykoEditorTags(element);
+  }
 }
 
-setTykoClasses()
+setTykoClasses();
