99fbbc05a631bb667f6149f25b7c7984919729ab
henry <henryborchers@yahoo.com>
2022-02-16 13:36:55 -0600

    formatting

--- tyko/static/js/editors.mjs
+++ tyko/static/js/editors.mjs
@@ -1,4 +1,4 @@
-import {Modal} from "bootstrap";
+import {Modal} from 'bootstrap';
 
 /**
  *
@@ -8,51 +8,74 @@ import {Modal} from "bootstrap";
  * @param {HTMLSelectElement} noteTypeElement
  * @param {HTMLTextAreaElement} noteMessageElement
  */
-export function configureNoteEditor(apiUrl, modalElement, noteIdElement, noteTypeElement, noteMessageElement) {
-    console.log("loading notes for " + apiUrl);
-
-    fetch(apiUrl)
-        .then((response)=>response.json())
-        .then((json)=>{
-            const modal = new Modal(modalElement);
-            noteIdElement.value = json['note_id'];
-            noteTypeElement.value = json['note_type_id'];
-            noteMessageElement.value = json['text'];
-            modal.show();
-        })
+export function configureNoteEditor(
+    apiUrl,
+    modalElement,
+    noteIdElement,
+    noteTypeElement,
+    noteMessageElement) {
+  fetch(apiUrl)
+      .then((response)=>response.json())
+      .then((json)=>{
+        const modal = new Modal(modalElement);
+        noteIdElement.value = json['note_id'];
+        noteTypeElement.value = json['note_type_id'];
+        noteMessageElement.value = json['text'];
+        modal.show();
+      });
 }
 
+/**
+ * Confirm removal of item.
+ */
 export class RemoveConfirm {
-    #modal;
-    #rootElement;
-    confirmButton = null;
-    onAccepted = function (){console.log('here')};
+  #modal;
+  #rootElement;
+  confirmButton = null;
+  onAccepted = function() {
+    console.log('here');
+  };
 
-    /**
-     *
-     * @param {HTMLDivElement} element
-     */
-    constructor(element) {
-        this.#rootElement = element
-        this.#modal = new Modal(element)
-    }
-    setTitle(message){
-        this.#rootElement.querySelector('.modal-title').innerHTML = message
-    }
+  /**
+   *
+   * @param {HTMLDivElement} element
+   */
+  constructor(element) {
+    this.#rootElement = element;
+    this.#modal = new Modal(element);
+  }
 
-    /**
-     *
-     * @param {HTMLButtonElement} element
-     */
-    setConfirmButton(element){
-        this.confirmButton = element;
-        let self = this;
-        element.onclick = function (){self.accept()};
-    }
-    accept(){
-        this.onAccepted();
-    }
-    show(){
-        this.#modal.show();
-    }
+  /**
+   * Set title message.
+   * @param {String} message
+   */
+  setTitle(message) {
+    this.#rootElement.querySelector('.modal-title').innerHTML = message;
+  }
+
+  /**
+   *
+   * @param {HTMLButtonElement} element
+   */
+  setConfirmButton(element) {
+    this.confirmButton = element;
+    const self = this;
+    element.onclick = function() {
+      self.accept();
+    };
+  }
+
+  /**
+   * Accept.
+   */
+  accept() {
+    this.onAccepted();
+  }
+
+  /**
+   * Show modal dialog.
+   */
+  show() {
+    this.#modal.show();
+  }
 }
