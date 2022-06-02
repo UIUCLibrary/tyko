859acb345fd4eb78865558e3e397884fe8a2c4fb
henry <henryborchers@yahoo.com>
2022-06-02 08:58:17 -0500

    documentation

--- tyko/utils.py
+++ tyko/utils.py
@@ -57,17 +57,19 @@ def serialize_precision_datetime(
 
 
 class AbsGetVersionStrategy(abc.ABC):
+    """Base class for getting version info."""
+
     @abc.abstractmethod
     def get_version(self):
-        """Get version"""
+        """Get version."""
 
 
 class StrategyError(Exception):
-    """Strategy error"""
+    """Strategy error."""
 
 
 class NoValidStrategy(StrategyError):
-    """No valid strategy"""
+    """No valid strategy."""
 
 
 class InvalidVersionStrategy(StrategyError):
@@ -75,21 +77,28 @@ class InvalidVersionStrategy(StrategyError):
 
 
 class PkgResourceDistributionVersionStrategy(AbsGetVersionStrategy):
+    """Use pkg_resources to locate version info from package metadata."""
+
     @staticmethod
-    def get_from_pkg_resources():
+    def get_from_pkg_resources() -> str:
+        """Get pky version with pkg_resources."""
         import pkg_resources
         try:
             return pkg_resources.get_distribution("tyko").version
         except pkg_resources.DistributionNotFound as error:
             raise InvalidVersionStrategy from error
 
-    def get_version(self):
+    def get_version(self) -> str:
+        """Get version info from pkg."""
         return self.get_from_pkg_resources()
 
 
 class GitVersionStrategy(AbsGetVersionStrategy):
+    """Version strategy using git version control."""
+
     @staticmethod
     def get_git_commit() -> bytes:
+        """Get git hash of HEAD."""
         git_command = shutil.which("git")
         if git_command is None:
             raise InvalidVersionStrategy("git command not found")
@@ -101,13 +110,30 @@ class GitVersionStrategy(AbsGetVersionStrategy):
             ]
         )
 
-    def get_version(self):
-        return str(self.get_git_commit())
+    def get_version(self) -> str:
+        """Get a git commit hash for a version."""
+        git_hash = str(self.get_git_commit())
+        return f"GIT:{git_hash}"
 
 
 def get_version(
         strategies: typing.List[typing.Type[AbsGetVersionStrategy]] = None
-):
+) -> str:
+    """Get the version information for Tyko.
+
+    This will cycle through the strategies and use the first one that does not
+    raise a InvalidVersionStrategy exception.
+
+    If all strategies fails to get a version a NoValidStrategy exception with
+    be raised.
+
+    Args:
+        strategies: Strategy in order to use for getting version info.
+
+    Returns:
+        version info as a string
+
+    """
     strategies = strategies or [
         PkgResourceDistributionVersionStrategy
     ]
