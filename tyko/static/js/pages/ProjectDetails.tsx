4a592509a7d92363f2b88736f2caaab6ec7b6e71
Henry Borchers <henryborchers@yahoo.com>
2022-07-04 21:57:08 -0500

    new object dialog created from project details in react page

--- tyko/static/js/pages/ProjectDetails.tsx
+++ tyko/static/js/pages/ProjectDetails.tsx
@@ -1,12 +1,21 @@
 import {LoadingIndeterminate} from '../reactComponents/Common';
 import Panel from '../reactComponents/Panel';
 import {Link, useParams} from 'react-router-dom';
-import {ButtonGroup, Col, Container, Form, Row} from 'react-bootstrap';
-import React, {useEffect, useState, FC} from 'react';
+import {
+  ButtonGroup,
+  CloseButton,
+  Col,
+  Container,
+  Form,
+  Row,
+} from 'react-bootstrap';
+import React, {useEffect, useState, FC, useRef} from 'react';
 import {EditableField} from '../reactComponents/ItemApp';
 import axios from 'axios';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import {ApiEnum, SelectDate, sortNameAlpha} from '../reactComponents/Items';
 
 const updateData = async (url: string, key: string, value: string) => {
   const data: {[key: string]: string} = {};
@@ -36,9 +45,145 @@ interface IProjectApi {
   }
 }
 
+interface ICollection {
+        collection_id: number
+      collection_name: string
+}
+interface ICollectionsApi {
+  collections: ICollection[];
+
+}
+
+export const NewObjectModal: FC<NewObjectModalProps> = (
+    {show, onAccepted, onClosed},
+)=>{
+  const [isOpen, setIsOpen] = useState(false);
+  const [collections, setCollections] = useState<ICollection[]| null>(null);
+  const newItemDialog = useRef(null);
+  useEffect(()=>{
+    setIsOpen(show);
+  },
+  [show],
+  );
+  useEffect(()=>{
+    const fetchData = async (url: string) => {
+      const data = ((await axios.get(url)).data as ICollectionsApi);
+      return data.collections;
+    };
+    fetchData('/api/collection')
+        .then(setCollections)
+        .catch(console.error);
+  }, []);
+  const handleClose = () => {
+    setIsOpen(false);
+    if (onClosed) {
+      onClosed();
+    }
+  };
+
+  const handleSubmit = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    if (onAccepted) {
+      onAccepted(event);
+    }
+  };
+  let form;
+  if (collections) {
+    const collectionsOptions = collections.map((collection) =>{
+      return (
+        <option
+          key={collection.collection_id}
+          value={collection.collection_id}>
+          {collection.collection_name}
+        </option>
+      );
+    });
+    form = (
+      <>
+        <Form.Group className="mb-3 row">
+          <Form.Label className="col-sm-2 col-form-label">Name</Form.Label>
+          <Form.Group className="col-sm-10">
+            <Form.Control name="name" required/>
+          </Form.Group>
+        </Form.Group>
+        <Form.Group className="mb-3 row">
+          <Form.Label className="col-sm-2 col-form-label">
+                Collection
+          </Form.Label>
+          <Form.Group className="col-sm-10">
+            <Form.Select name="collection_id" required>
+              <option value="" disabled selected>Select a collection
+              </option>
+              {collectionsOptions}
+            </Form.Select>
+          </Form.Group>
+        </Form.Group>
+        <Form.Group className="mb-3 row">
+          <Form.Label className="col-sm-2 col-form-label">
+            Barcode
+          </Form.Label>
+          <Form.Group className="col-sm-10">
+            <Form.Control name="objectBarcode"/>
+          </Form.Group>
+        </Form.Group>
+        <Form.Group className="mb-3 row">
+          <Form.Label className="col-sm-2 col-form-label">
+                Originals Received
+          </Form.Label>
+          <Form.Group className="col-sm-10">
+            <SelectDate
+              name='originals_rec_date'
+              dateFormat='m/dd/yyyy'
+            />
+          </Form.Group>
+        </Form.Group>
+      </>
+    );
+  } else {
+    form = <LoadingIndeterminate/>;
+  }
+  const modalFooter = collections ? (
+    <ButtonGroup>
+      <Button variant='danger' onClick={handleClose}>Cancel</Button>
+      <Button variant="primary" type="submit">Create</Button>
+    </ButtonGroup>
+  ) : (
+      <ButtonGroup>
+        <Button variant='danger' onClick={handleClose}>Cancel</Button>
+      </ButtonGroup>
+  );
+  return (
+    <Modal
+      data-testid='newObjectModal'
+      show={isOpen}
+      onHide={handleClose}
+      size="lg"
+      ref={newItemDialog}>
+      <Modal.Header>
+        <h5 className="modal-title" id="titleId">New Object</h5>
+        <CloseButton
+          aria-label="Close"
+          onClick={handleClose}
+        />
+      </Modal.Header>
+      <form id="formId" title='newObject' onSubmit={handleSubmit}>
+        <Modal.Body>
+          {form}
+        </Modal.Body>
+        <Modal.Footer>{modalFooter}</Modal.Footer>
+      </form>
+    </Modal>
+  );
+};
+
 interface IProjectDetails {
   apiData: IProjectApi
   apiUrl: string
+  onUpdated: ()=>void
+}
+interface IProjectObjectDetails {
+  apiData: IProjectApi
+  submitUrl: string
   onUpdated: ()=>void
 }
 
@@ -111,11 +256,36 @@ const ProjectDetailDetails: FC<IProjectDetails> = (
   );
 };
 
-const ProjectObjects: FC<IProjectDetails> = (
-    {apiData, apiUrl, onUpdated},
+interface NewObjectModalProps{
+  show: boolean,
+  onAccepted?: (event: React.SyntheticEvent)=>void
+  onClosed?: ()=>void
+}
+
+const ProjectObjects: FC<IProjectObjectDetails> = (
+    {apiData, submitUrl, onUpdated},
 ) =>{
+  const [
+    newObjectDialogShown,
+    setNewObjectDialogShown,
+  ] = useState<boolean>(false);
   const handleCreateObject = () =>{
-    console.log('Create a new object');
+    setNewObjectDialogShown(true);
+  };
+  const handleAcceptedNewObject = (event: React.SyntheticEvent) => {
+    const formData = new FormData(event.target as HTMLFormElement);
+    const formProps = Object.fromEntries(formData);
+    axios.post(submitUrl, formProps)
+        .then(()=>{
+          setNewObjectDialogShown(false);
+          if (onUpdated) {
+            onUpdated();
+          }
+        })
+        .catch(console.error);
+  };
+  const handleClosedNewDialogBox = () => {
+    setNewObjectDialogShown(false);
   };
   const rows = apiData.project.objects.map((data, index) => {
     return (
@@ -138,9 +308,14 @@ const ProjectObjects: FC<IProjectDetails> = (
     </tbody>
   </Table>;
   return (<>
+    <NewObjectModal
+      show={newObjectDialogShown}
+      onAccepted={handleAcceptedNewObject}
+      onClosed={handleClosedNewDialogBox}
+    />
     {table}
     <ButtonGroup className="float-end">
-      <Button onClick={handleCreateObject}>Create</Button>
+      {<Button onClick={handleCreateObject}>Add</Button>}
     </ButtonGroup>
   </>);
 };
@@ -158,6 +333,8 @@ export default function ProjectDetails() {
   };
 
   const apiUrl = projectId? `/api/project/${projectId}` : '';
+  const submitNewObjectUrl =
+      projectId ? `/api/project/${projectId}/object` : '';
 
   useEffect(()=>{
     if (!loading) {
@@ -182,7 +359,7 @@ export default function ProjectDetails() {
       onUpdated={()=>setApiData(null)}/>;
     objectsPanel = <ProjectObjects
       apiData={apiData}
-      apiUrl={apiUrl}
+      submitUrl={submitNewObjectUrl}
       onUpdated={()=>setApiData(null)}/>;
     notesPanel = <>do stuff here</>;
   }
