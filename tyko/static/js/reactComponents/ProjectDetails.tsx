17b657d460cfc4037be3e9e44f102103df6c0784
henry <henryborchers@yahoo.com>
2022-07-14 11:13:48 -0500

    use a single window for editing project details

--- tyko/static/js/reactComponents/ProjectDetails.tsx
+++ tyko/static/js/reactComponents/ProjectDetails.tsx
@@ -1,7 +1,8 @@
-import React, {FC, useId, useReducer} from 'react';
-import {Button, ButtonGroup, Col, Form, Row} from 'react-bootstrap';
-import {EditableField} from './ItemApp';
+import React, {FC, useId, useReducer, useState} from 'react';
+import {Button, ButtonGroup, Form} from 'react-bootstrap';
 import axios from 'axios';
+import {LoadingIndeterminate} from './Common';
+import {InactiveCover} from './Panel';
 interface IObject {
   barcode: string|null
   collection_id: number
@@ -77,97 +78,92 @@ export const ProjectDetailDetails: FC<IProjectDetails> = (
     {apiData, apiUrl, onUpdated},
 ) => {
   const [editMode, setEditMode] = useReducer((mode)=>!mode, false);
-
+  const [accessible, setAccessible] = useState(true);
   const handleUpdate = (event: React.SyntheticEvent)=> {
     event.preventDefault();
     const formData = new FormData(event.target as HTMLFormElement);
     const formProps = Object.fromEntries(formData);
+    setAccessible(false);
     axios.put(apiUrl, formProps).then(()=>{
+      setAccessible(true);
       if (onUpdated) {
         onUpdated();
       }
     }).catch(console.error);
   };
-
   return (
-    <Form onSubmit={handleUpdate}>
-      <Form.Group className="mb-3 row">
-        <ProjectDetailsRow
-          label='Title'
-          editorId='titleField'
-          display={apiData.project.title}
-          editMode={editMode}
-        >
-          <Form.Control
-            id="titleField"
-            name='title'
-            defaultValue={apiData.project.title}
-          />
-        </ProjectDetailsRow>
-        <ProjectDetailsRow
-          label='Project Code'
-          editorId='projectCodeField'
-          display={apiData.project.project_code}
-          editMode={editMode}
-        >
-          <Form.Control
-            id="projectCodeField"
-            name='project_code'
-            defaultValue={apiData.project.project_code}
-          />
-        </ProjectDetailsRow>
-        <ProjectDetailsRow
-          label='Status'
-          editorId='currentStatusField'
-          display={apiData.project.status}
-          editMode={editMode}>
-          <Form.Select
-            id="currentStatusField"
-            name='status'
-            defaultValue={apiData.project.status}
-          >
-            <option value=''></option>
-            <option value='No Work Done'>No Work Done</option>
-            <option value='In Progress'>In Progress</option>
-            <option value='Complete'>Complete</option>
-          </Form.Select>
-        </ProjectDetailsRow>
-        {/* <Row>*/}
-        {/*  <Col sm={2}>*/}
-        {/*    <Form.Label>Status</Form.Label>*/}
-        {/*  </Col>*/}
-        {/*  <Col>*/}
-        {/*    <EditableField*/}
-        {/*      display={apiData.project.status}*/}
-        {/*      onSubmit={(value)=> {*/}
-        {/*        updateData(apiUrl, 'status', value)*/}
-        {/*            .then(()=> onUpdated())*/}
-        {/*            .catch(console.error);*/}
-        {/*      }}*/}
-        {/*    />*/}
-        {/*  </Col>*/}
-        {/* </Row>*/}
-        <ProjectDetailsRow
-          label='Current Location'
-          editorId='currentLocationField'
-          display={apiData.project.current_location}
-          editMode={editMode}>
-          <Form.Control
-            id="currentLocationField"
-            name='current_location'
-            defaultValue={apiData.project.current_location}
-          />
-        </ProjectDetailsRow>
-      </Form.Group>
-      <ButtonGroup hidden={!editMode}>
-        <Button variant={'outline-danger'} onClick={setEditMode}>
+    <div id='outer'>
+      {
+        accessible ?
+          <></> :
+          <InactiveCover><LoadingIndeterminate/></InactiveCover>
+      }
+      <Form onSubmit={handleUpdate}>
+        <div>
+          <Form.Group className="mb-3 row">
+            <ProjectDetailsRow
+              label='Title'
+              editorId='titleField'
+              display={apiData.project.title}
+              editMode={editMode}
+            >
+              <Form.Control
+                id="titleField"
+                name='title'
+                defaultValue={apiData.project.title}
+              />
+            </ProjectDetailsRow>
+            <ProjectDetailsRow
+              label='Project Code'
+              editorId='projectCodeField'
+              display={apiData.project.project_code}
+              editMode={editMode}
+            >
+              <Form.Control
+                id="projectCodeField"
+                name='project_code'
+                defaultValue={apiData.project.project_code}
+              />
+            </ProjectDetailsRow>
+            <ProjectDetailsRow
+              label='Status'
+              editorId='currentStatusField'
+              display={apiData.project.status}
+              editMode={editMode}>
+              <Form.Select
+                id="currentStatusField"
+                name='status'
+                defaultValue={apiData.project.status}
+              >
+                <option value=''></option>
+                <option value='No Work Done'>No Work Done</option>
+                <option value='In Progress'>In Progress</option>
+                <option value='Complete'>Complete</option>
+              </Form.Select>
+            </ProjectDetailsRow>
+            <ProjectDetailsRow
+              label='Current Location'
+              editorId='currentLocationField'
+              display={apiData.project.current_location}
+              editMode={editMode}>
+              <Form.Control
+                id="currentLocationField"
+                name='current_location'
+                defaultValue={apiData.project.current_location}
+              />
+            </ProjectDetailsRow>
+          </Form.Group>
+        </div>
+        <ButtonGroup hidden={!editMode}>
+          <Button variant={'outline-danger'} onClick={setEditMode}>
             Cancel
-        </Button>
-        <Button type='submit' variant={'outline-primary'}>
+          </Button>
+          <Button type='submit' variant={'outline-primary'}>
             Confirm
-        </Button>
-      </ButtonGroup>
-      <Button hidden={editMode} onClick={setEditMode}>Edit</Button>
-    </Form>
+          </Button>
+        </ButtonGroup>
+        <Button hidden={editMode} onClick={setEditMode}>Edit</Button>
+      </Form>
+    </div>
   );
 };
