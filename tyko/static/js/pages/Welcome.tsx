c764083a88ea864427ac29dd3fc4e88cf2c2eb34
henry <henryborchers@yahoo.com>
2022-07-14 13:53:33 -0500

    added welcome screen to index

---
+++ tyko/static/js/pages/Welcome.tsx
@@ -0,0 +1,14 @@
+/**
+ * Welcome screen.
+ *
+ * Designed to be the index.
+ * @constructor
+ */
+export default function Welcome() {
+  return (
+    <>
+      <h1 className="mt-5">Tyko</h1>
+      <p className="lead text-muted">Please select an option from the menu</p>
+    </>
+  );
+}
