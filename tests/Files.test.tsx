b4cf48d2903d423b6ccf8f3e72df33a219430c75
henry <henryborchers@yahoo.com>
2022-09-07 16:03:25 -0500

    test edit mode switching

--- tests/Files.test.tsx
+++ tests/Files.test.tsx
@@ -5,11 +5,22 @@
 'use strict';
 import '@testing-library/jest-dom';
 import {Files} from '../tyko/static/js/reactComponents/Files';
-import {render, screen} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 
 describe('Files', ()=> {
   test('Edit button', ()=>{
     render(<Files apiUrl='/foo'/>);
+    expect(screen.getByText('Edit')).toBeVisible();
+  });
+  test('edit mode switches', ()=>{
+    render(<Files apiUrl='/foo'/>);
+    fireEvent.click(screen.getByText('Edit'));
+    expect(screen.getByText('Edit')).not.toBeVisible();
+  });
+  test('edit mode switches back', ()=>{
+    render(<Files apiUrl='/foo'/>);
+    fireEvent.click(screen.getByText('Edit'));
+    fireEvent.click(screen.getByText('Done'));
     expect(screen.getByText('Edit')).toBeVisible();
   });
 });
